953b52f9f3e17617c8c8cb470469b72729c0aea1
chrislee <chrile@34159e166bde.ant.amazon.com>
2014-07-27 17:20:44 -0700

    Here is my work with variables and other shenanigans

---
+++ Exercise11.java
@@ -0,0 +1,18 @@
+public class Exercise11
+{
+	public static void main( String[] args )
+	{
+		int room;
+		double e;
+		String cs;
+		
+		room = 113;
+		e = 2.71828;
+		cs = "Computer Science";
+		
+		System.out.println( "This is room #" + room );
+		System.out.println( "e is close to " + e );
+		System.out.println( "I am learning a bit about " + cs );
+		
+	}
+}
---
+++ Exercise12.java
@@ -0,0 +1,13 @@
+public class Exercise12
+{
+	public static void main( String[] args )
+	{
+		int gradYear;
+		String name;
+		
+		name = "Chris Lee";
+		gradYear = 2014;
+		
+		System.out.println( "My name is " + name + " and I'll graduate in " + gradYear + "." );
+	}
+}
